abc55038bfd4aba2674de41f0dcaa72a127b99a6
raknos13 <49271412+raknos13@users.noreply.github.com>
2020-07-21 15:12:20 +0530

    Update i3-gaps.sh
    added few more dependencies

--- i3-gaps.sh
+++ i3-gaps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev \
+	libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-shape0-dev
 
 cd /tmp
 
