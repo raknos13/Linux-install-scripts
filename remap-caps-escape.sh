91d8475892cbc911af5ad9c2d5bebec572321ef8
raknos13 <49271412+raknos13@users.noreply.github.com>
2021-04-11 17:00:28 +0530

    Update remap-caps-escape.sh

--- remap-caps-escape.sh
+++ remap-caps-escape.sh
@@ -3,4 +3,6 @@
 # Remap Caps_Lock to Escape; and
 # Remap Shift+Caps_Lock as Caps_Lock
 
+# Add command in ~/.xinitrc or ~/.xsession or add script to autostart.
+
 setxkbmap -option caps:escape_shifted_capslock
