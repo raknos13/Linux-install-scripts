939547aec384690ae98e1407dac42637a9002fc1
Mukesh Kumar <mkso13189@gmail.com>
2021-04-23 18:37:14 +0530

    made installer executable and changed its name.

---
+++ Install-Canon-lbp2900/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#yay -S captdriver-git capt-src --noconfirm
+pamac build captdriver-git capt-src --no-confirm
+
+systemctl enable cups.service
+systemctl start cups.service
+
+#git clone https://github.com/raknos13/Linux-install-scripts.git
+#cd Linux-install-scripts/Install-Canon-lbp2900/
+sudo lpadmin -p LBP2900 -m CNCUPSLBP2900CAPTK.ppd -v ccp://localhost:59687 -E
+#cd ../..
+#rm -rf Linux-install-scripts/
+ccpdadmin -p LBP2900 -o /dev/usb/lp0
+systemctl enable ccpd.service
+systemctl start ccpd.service
+
+
+
